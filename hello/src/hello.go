da3af0974a1086f068a0f58eccada9824f32b2b8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-08-16 14:28:21 -0300

    Alterando arquivo da aula 2 do curso Go: Iniciando com a linguagem do Google

--- hello/src/hello.go
+++ hello/src/hello.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Olá mundo!")
-	fmt.Println("Este é o meu primeiro programa em Go.")
+
+	//Atribuindo valores as variaveis
+	nome := "Henrique"
+	idade := 20
+	versao := 1.1
+
+	//Imprimindo os valores das variaveis
+	fmt.Println("Olá", nome)
+	fmt.Println("sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	//Imprimindo os tipos das variaveis
+	fmt.Println("O tipo da variável nome é:", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável idade é:", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variável versao é:", reflect.TypeOf(versao))
 }

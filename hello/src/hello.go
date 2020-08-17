19b1db4e9d6941c9ad8cc2c8832377153814b8c8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-08-17 19:00:24 -0300

    Alterando arquivos da aula 2 do curso Go: Iniciando com a linguagem do Google

--- hello/src/hello.go
+++ hello/src/hello.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 
-	//Atribuindo valores as variaveis
-	nome := "Henrique"
-	idade := 20
-	versao := 1.1
+	//Exibindo mensagem de bem vindo
+	fmt.Println("Bem vindo ao monitor de websites.")
 
-	//Imprimindo os valores das variaveis
-	fmt.Println("Olá", nome)
-	fmt.Println("sua idade é", idade)
-	fmt.Println("Este programa está na versão", versao)
+	//Exibindo mensagem pedindo para o usuario escolher o que deseja fazer
+	fmt.Println("O que deseja fazer: ")
+	fmt.Println("0 - Sair do programa")
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir os logs")
 
-	//Imprimindo os tipos das variaveis
-	fmt.Println("O tipo da variável nome é:", reflect.TypeOf(nome))
-	fmt.Println("O tipo da variável idade é:", reflect.TypeOf(idade))
-	fmt.Println("O tipo da variável versao é:", reflect.TypeOf(versao))
+	//Lendo a entrada do usuario
+	var comando int8
+	//fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+
+	//Imprimindo dados da variavel comando
+	fmt.Println("O endereço da variável comando é: ", &comando)
+	fmt.Println("O comando escolhido foi", comando)
+
 }

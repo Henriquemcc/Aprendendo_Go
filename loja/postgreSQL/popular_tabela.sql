3c0da316bff967baeff504b327d3f871a42829de
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-08-27 14:04:40 -0300

    Adicionando arquivos da aula 2 do curso Go: Fundamentos de uma aplicação web

---
+++ loja/postgreSQL/popular_tabela.sql
@@ -0,0 +1,6 @@
+insert into produtos (nome, descricao, preco, quantidade) values
+('Camiseta', 'Azul', 20.00, 5),
+('Computador Gamer', 'Com 12 núcleos de CPU e 2048 núcleos de GPU.', 30000.00, 4),
+('Console de Video Game', 'De ultima geração com 2TB de SSD.', 8000.00, 8),
+('Smartphone', 'Com Android 11', 5000.00, 12),
+('Roteador WiFi', 'Mesh Dual Band 2,4Ghz e 5Ghz com IEEE 802.11b/g/n/ac e compatível com IPv6.', 2000.00, 6)

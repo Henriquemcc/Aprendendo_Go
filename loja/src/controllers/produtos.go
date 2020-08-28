af7d1958d9f0dfc6f418c5bee2af9d75c2fe005a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-08-28 20:01:24 -0300

    Adicionando e alterando arquivos da aula 3 do curso Go: Fundamentos de uma aplicação web

--- loja/src/controllers/produtos.go
+++ loja/src/controllers/produtos.go
@@ -9,7 +9,7 @@ import (
 //Criando o template da aplicacao web
 var templateDaAplicacaoWeb = template.Must(template.ParseGlob("templates/*.html"))
 
-//Esta funcao serve para atender a requisicao '/'
+//Index serve para atender a requisicao para a pagina Index, que mostra os produtos da loja.
 //Parametro: w: Instancia da interface usada pelo manipulador http para construir a resposta http.
 //Parametro: r: Instancia da struct request, que representa uma requisicao recebida pelo servidor ou enviada pelo cliente.
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +17,15 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	//Obtendo a lista de todos os produtos
 	listaDeProdutos := models.BuscarTodosProdutos()
 
-	//Executando aplicacao web
+	//Executando aplicacao web para a pagina Index
 	templateDaAplicacaoWeb.ExecuteTemplate(w, "Index", listaDeProdutos)
+}
+
+//New serve para atender a requisicao para a pagina New, que permite adicionar produtos na loja.
+//Parametro: w: Instancia da interface usada pelo manipulador http para construir a resposta http.
+//Parametro: r: Instancia da struct request, que representa uma requisicao recebida pelo servidor ou enviada pelo cliente.
+func New(w http.ResponseWriter, r *http.Request) {
+
+	//Executand aplicacao web para a pagina New
+	templateDaAplicacaoWeb.ExecuteTemplate(w, "New", nil)
 }
--- loja/src/routes/routes.go
+++ loja/src/routes/routes.go
@@ -7,5 +7,10 @@ import (
 
 //CarregarRotas serve para carregar as rotas http do servidor web
 func CarregarRotas() {
+
+	//Carregando rota para a pagina index
 	http.HandleFunc("/", controllers.Index)
+
+	//Carregando rota para a pagina new
+	http.HandleFunc("/new", controllers.New)
 }
